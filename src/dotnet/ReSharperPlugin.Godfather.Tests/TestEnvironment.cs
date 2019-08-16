7f68b26873795b970503e05c24ab86ab4b2a2792
Rixium <gamerpotty@gmail.com>
2019-08-16 13:11:05 +0100

    Initial commit

---
+++ src/dotnet/ReSharperPlugin.Godfather.Tests/TestEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: RequiresSTA]
+
+namespace ReSharperPlugin.Godfather.Tests
+{
+  [ZoneDefinition]
+  public interface IGodfatherTestZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>
+  {
+  }
+
+  [SetUpFixture]
+  public class TestEnvironment : ExtensionTestEnvironmentAssembly<IGodfatherTestZone>
+  {
+  }
+}
